0f252304e8b7d794590ee982a735304e94c56ce8
mvfflin <mynameisfatihul@gmail.com>
2024-09-02 23:49:54 +0700

    feat: update roll system

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,55 +1,92 @@
 "use client";
 
+import { auraList } from "@/constants/dummyaura";
 import Image from "next/image";
 import { FormEvent, useState } from "react";
 
 export default function Home() {
-    const [aura, setAura] = useState(0);
+    const [aura, setAura] = useState<any>(0);
+    // const [totalaura, setTotalAura] = useState(0)
     const [cd, setCD] = useState(false);
     const DISABLED_CLASS = "disabled";
     const UPDATE_INTERVAL = 1000 / 60; // 60 times/s 60 FPS
+    const [cumul, setCumul] = useState<any>();
+    const [rand, setRand] = useState<any>();
+    let TotalAura = 0;
 
+    auraList.map((aura, index) => {
+        TotalAura += aura.Rarity;
+        console.log(TotalAura);
+    });
+    
     const rollAura = (event: any) => {
         const button = document.getElementById("rollAura");
-
-        if (!button) return;
-
+        
+        if (!button || cd) return;
+        
+        let randomChance = Math.floor(Math.random() * TotalAura);
+        
+        let Cumulative = 0;
+        for (let index = 0; index < auraList.length; index++) {
+            Cumulative += auraList[index].Rarity;
+            setCumul(Cumulative);
+            setRand(randomChance);
+            if (randomChance <= Cumulative) {
+                setAura(auraList[index].Name);
+                break;
+                // return auraList[index].Name;
+            }
+        }
+        setCD(true);
+        // setAura(auraname);
+        // setAura((prev) => prev + 1);
         button.classList.add(DISABLED_CLASS);
         button.style.cssText = "--time-left: 100%;";
 
-        let time = 500 - UPDATE_INTERVAL;
-        const intervalID = () => {
-            const passedTime = (time / 500) * 100;
+        let time = 1000 - UPDATE_INTERVAL;
+        const intervalID = setInterval(() => {
+            const passedTime = (time / 1000) * 100;
             button.style.cssText = `--time-left: ${passedTime}%;`;
 
-            button.textContent;
-        };
+            button.innerHTML = (time / 1000).toFixed(2);
+            time -= UPDATE_INTERVAL;
+
+            if (time < 0) {
+                button.innerText = "Roll Aura";
+                // button.style.cssText = "";
+                button.classList.remove(DISABLED_CLASS);
+                setCD(false);
+                clearInterval(intervalID);
+            }
+        }, UPDATE_INTERVAL);
     };
 
     return (
         <main className="">
-            <h1 className="text-center py-10 text-2xl">Aura : {aura}</h1>
+            <h1 className="text-center py-10 text-2xl">
+                Aura : {aura} | {rand} | {cumul}
+            </h1>
             <div className="justify-center">
                 <div
+                    id="rollAura"
                     onClick={(e) => {
                         if (cd == false) {
-                            setAura((prev) => prev + 1);
-                            setCD(true);
-                            setTimeout(() => {
-                                setCD(false);
-                            }, 500);
+                            // setAura((prev) => prev + 1);
+                            // setCD(true);
+                            // setTimeout(() => {
+                            //     setCD(false);
+                            // }, 500);
+                            rollAura(e);
                         }
                     }}
                     className="button w-40 h-16 bg-blue-500 rounded-lg cursor-pointer select-none
     active:translate-y-2  active:[box-shadow:0_0px_0_0_#1b6ff8,0_0px_0_0_#1b70f841]
     active:border-b-[0px]
     transition-all duration-150 [box-shadow:0_10px_0_0_#1b6ff8,0_15px_0_0_#1b70f841]
-    border-b-[1px] border-blue-400 mx-auto 
+    border-b-[1px] border-blue-400 mx-auto text-white text-center font-bold justify-center items-center py-5
   "
                 >
-                    <span className="flex flex-col justify-center items-center h-full text-white font-bold text-lg ">
-                        +1 Aura
-                    </span>
+                    Roll Aura
                 </div>
             </div>
         </main>
---
+++ src/constants/dummyaura.ts
@@ -0,0 +1,27 @@
+export const auraList = [
+    {
+        Name: "Sigma",
+        Rarity: 1,
+    },
+    {
+        Name: "Skibidi",
+        Rarity: 2,
+    },
+    {
+        Name: "Epic",
+        Rarity: 5000,
+    },
+    {
+        Name: "Rare",
+        Rarity: 9990,
+    },
+    {
+        Name: "Common",
+        Rarity: 10000,
+    },
+    // "Sigma" = 1,
+    // "Skibidi": 15,
+    // "Epic": 9970,
+    // "Rare": 9995,
+    // "Common": 9999
+];
